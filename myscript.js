13b87a1c9a0e7bcd0655f8fa6d672ceae0a32166
M1-73 أمل <76258869+am971@users.noreply.github.com>
2021-01-07 23:37:23 +0300

    done

--- myscript.js
+++ myscript.js
@@ -1,14 +1,47 @@
 let board = [
-    ['00', '01', '02'],
-    ['10', '11', '12'],
-    ['20', '21', '22']
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
 ]
 
+let turn = "O";
 
-
-function press(cellNO) {
-
+function press(x,y) {
     // Do stuff
+    if ( turn == "O"){ turn =  "X" ; document.getElementById("tic").play() }
+    else { turn ="O";document.getElementById("tic").play()}
+    board [x][y] = turn;
+    document.getElementById(`${x}${y}`).innerText = turn;
+    if ( board [0][0] == turn && board [0][1] == turn && board [0][2]== turn  ) { 
+             document.getElementById("status").innerText = ` ${turn} win`; }
+    if ( board [0][0] == turn && board [1][1] == turn && board [2][2]== turn  ) { 
+        document.getElementById("status").innerText = ` ${turn} win`; }
+    if ( board [0][2] == turn && board [1][1] == turn && board [2][0]== turn  ) { 
+        document.getElementById("status").innerText = ` ${turn} win`; }
+    if ( board [1][0] == turn && board [1][1] == turn && board [1][2]== turn  ) { 
+        document.getElementById("status").innerText = ` ${turn} win`; }
+    if ( board [2][0] == turn && board [2][1] == turn && board [2][2]== turn  ) { 
+         document.getElementById("status").innerText = ` ${turn} win`; } 
+   if ( board [0][0] == turn && board [1][0] == turn && board [2][0]== turn  ) { 
+        document.getElementById("status").innerText = ` ${turn} win`; }
+    if ( board [0][1] == turn && board [1][1] == turn && board [2][1]== turn  ) { 
+            document.getElementById("status").innerText = ` ${turn} win`; } 
+    if ( board [0][2] == turn && board [1][2] == turn && board [2][2]== turn  ) { 
+        document.getElementById("status").innerText = ` ${turn} win`; }           
+
     console.log(cellNO)
     
 }
+
+function resetb() {
+    document.getElementById("status").innerText="";
+    var tds = document.getElementsByTagName('td');
+    for(var i = 0; i <= tds.length; i++) {
+        tds[i].innerText = "" } 
+    
+            
+    
+
+
+  }
+  
